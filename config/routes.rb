645889d4bd961c1ab1f638a2cdd3e19e9d3d9da4
Saud <qahmad@chicagobooth.edu>
2017-07-19 03:31:16 +0000

    25% done

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 
   # Uncomment each route below ONE AT A TIME and debug.
 
-  # get("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram" })
-  # get("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
+  get("/zodiacs/aries",       { :controller =>  "zodiacs",  :action =>  "ram" })
+  get("/zodiacs/taurus",      { :controller =>  "zodiacs",  :action =>  "bull" })
+  get("/zodiacs/gemini",      { :controller =>  "zodiacs",  :action =>  "twins" })
   # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
   # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
   # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
