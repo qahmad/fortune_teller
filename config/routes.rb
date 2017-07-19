cbae4bf5f764a41373c2adb8347734f2e59871ca
Saud <qahmad@chicagobooth.edu>
2017-07-19 03:48:58 +0000

    100% done

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,9 @@ Rails.application.routes.draw do
   get("/zodiacs/libra",       { :controller =>  "zodiacs",  :action =>  "scales" })
   get("/zodiacs/scorpio",     { :controller =>  "zodiacs",  :action =>  "scorpion" })
   get("/zodiacs/sagittarius", { :controller =>  "zodiacs",  :action =>  "archer" })
-  # get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  # get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  get("/zodiacs/capricorn",   { :controller =>  "zodiacs",  :action =>  "goat" })
+  get("/zodiacs/aquarius",    { :controller =>  "zodiacs",  :action =>  "waterbearer" })
+  get("/zodiacs/pisces",      { :controller =>  "zodiacs",  :action =>  "fish" })
 
   mount WebGit::Engine, at: "/rails/git"
 end
